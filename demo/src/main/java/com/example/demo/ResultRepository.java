cd74240e3d7d467c2128d416b90fd95e8b333f14
unknown <josip.vukojaa@gmail.com>
2021-06-04 17:17:04 +0200

    commit

--- demo/src/main/java/com/example/demo/ResultRepository.java
+++ demo/src/main/java/com/example/demo/ResultRepository.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 import com.example.demo.Result;
 
-public interface ResultRepository extends JpaRepository<Result, UUID>{
+public interface ResultRepository extends JpaRepository<Result, String>{
     
 }
